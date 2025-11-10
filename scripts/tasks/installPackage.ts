4109b8742193499149b1596ca7bc7ea7716eb93d
gdlol <gdlol@users.noreply.github.com>
2025-11-10 16:04:20 +0000

    tests

---
+++ scripts/tasks/installPackage.ts
@@ -0,0 +1,45 @@
+import { cp, mkdir, rm, writeFile } from "node:fs/promises";
+import path from "node:path";
+
+import { $ } from "execa";
+
+import { packageOutputPath, packagePrefix } from "@/scripts/project.js";
+import { build } from "@/scripts/tasks/build.js";
+import pkg from "@/setup/package.json" with { type: "json" };
+
+export const installPackage = async () => {
+  const XDG_DATA_HOME = process.env.XDG_DATA_HOME;
+  if (!XDG_DATA_HOME) {
+    throw new Error("XDG_DATA_HOME is not set.");
+  }
+
+  await build();
+
+  // Copy the dist folder to a temporary location
+  const packageName = `${packagePrefix}${pkg.name}`;
+  const tempPath = `/tmp/${packageName}`;
+  await rm(tempPath, { recursive: true, force: true });
+  await mkdir(tempPath, { recursive: true });
+  await cp(packageOutputPath, tempPath, { recursive: true });
+  await writeFile(path.resolve(tempPath, "pnpm-workspace.yaml"), "");
+  await writeFile(
+    path.resolve(tempPath, ".npmrc"),
+    [
+      "lockfile=false",
+      "resolution-mode=time-based",
+      "shared-workspace-lockfile=false",
+      "inject-workspace-packages=true",
+    ].join("\n"),
+  );
+
+  // Install the package globally after pnpm deploy.
+  process.env.NODE_ENV = "production";
+  const installPath = path.resolve(XDG_DATA_HOME, pkg.name);
+  await rm(installPath, { recursive: true, force: true });
+  const $$ = $({ stdio: "inherit", verbose: "full", cwd: tempPath });
+  await $$`pnpm install`;
+  await $$`pnpm deploy --filter=${packageName} --prod ${installPath}`;
+  await $$({ reject: false })`pnpm uninstall --silent --global ${packageName}`;
+  await $$`pnpm install --global ${installPath}`;
+  await $$`rm --recursive ${tempPath}`;
+};
--- scripts/test.ts
+++ scripts/test.ts
@@ -1,1 +1,26 @@
-console.log("TODO");
+import { mkdir, rm } from "node:fs/promises";
+
+import { $ } from "execa";
+
+import { packagePrefix } from "@/scripts/project.js";
+import { installPackage } from "@/scripts/tasks/installPackage.js";
+
+await installPackage();
+
+const tempPath = `/tmp/${packagePrefix}/tests`;
+const $$ = $({ stdio: "inherit", verbose: "full", cwd: tempPath });
+
+const reset = async (path: string) => {
+  await rm(path, { recursive: true, force: true });
+  await mkdir(path, { recursive: true });
+};
+
+await reset(tempPath);
+await $$`create-devcontainer csharp`;
+await $$`devcontainer up --remove-existing-container --workspace-folder ${tempPath}`;
+await $$`devcontainer exec --workspace-folder ${tempPath} pnpm lint`;
+
+await reset(tempPath);
+await $$`create-devcontainer typescript`;
+await $$`devcontainer up --remove-existing-container --workspace-folder ${tempPath}`;
+await $$`devcontainer exec --workspace-folder ${tempPath} pnpm lint`;
