e7bfb3f39c3bf5e9d4cd4215f81a84a9a2b5075b
gdlol <gdlol@users.noreply.github.com>
2025-11-10 16:19:36 +0000

    cleanup

--- scripts/test.ts
+++ scripts/test.ts
@@ -5,9 +5,11 @@ import { $ } from "execa";
 import { packagePrefix } from "@/scripts/project.js";
 import { installPackage } from "@/scripts/tasks/installPackage.js";
 
+// Not to be run in CI.
+
 await installPackage();
 
-const tempPath = `/tmp/${packagePrefix}/tests`;
+const tempPath = `/tmp/${packagePrefix}tests`;
 const $$ = $({ stdio: "inherit", verbose: "full", cwd: tempPath });
 
 const reset = async (path: string) => {
@@ -24,3 +26,12 @@ await reset(tempPath);
 await $$`create-devcontainer typescript`;
 await $$`devcontainer up --remove-existing-container --workspace-folder ${tempPath}`;
 await $$`devcontainer exec --workspace-folder ${tempPath} pnpm lint`;
+
+// https://github.com/devcontainers/cli/issues/386
+await rm(`${tempPath}/.devcontainer/Dockerfile`);
+console.log("cleaning up...");
+await $({
+  cwd: tempPath,
+  reject: false,
+  stdio: "ignore",
+})`devcontainer up --remove-existing-container --workspace-folder ${tempPath}`;
