bcfd0536baac0f0c744c10f3d0258afb37661fe6
gdlol <gdlol@users.noreply.github.com>
2026-02-21 05:30:09 +0000

    update project configs.

--- scripts/publish.ts
+++
@@ -1,5 +0,0 @@
-import { packageOutputPath } from "@/scripts/project.js";
-import { $$ } from "@/scripts/shell.js";
-
-await $$`npm set //registry.npmjs.org/:_authToken=\${NPM_TOKEN}`;
-await $$`npm publish --access public ${packageOutputPath}`;
